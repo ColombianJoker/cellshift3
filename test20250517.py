8222cb6732fc93ea39276c77724535b509e00b35
ColombianJoker <elbarrios1@poligran.edu.co>
2025-05-17 18:31:23 -0500

    Organize module

---
+++ test20250517.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from cs import CS
+from IPython import display
+import sys
+
+try:
+  r1 = CS("seattle_pets.csv")
+  print(f"{type(r1)=}\n")
+  print(f"{r1=}\n")
+  print(f"{dir(r1)=}\n")
+  print(f"{type(r1.data)=}\n")
+  print(f"{r1.get_tablename()=}\n")
+  print(f"{type(r1.to_pandas())=}\n")
+  r1.to_csv("copy.csv")
+  df1 = r1.to_pandas()
+  print(f"{type(df1)=}\n")
+except Exception as e:
+  print(f"{e=}", file=sys.stderr)
